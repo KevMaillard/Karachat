14f467bc93badf8f38e6cc3eda12da93c141e10d
KevMaillard <86302957+KevMaillard@users.noreply.github.com>
2021-10-16 01:54:47 +0200

    Tequila

---
+++ app.js
@@ -0,0 +1,26 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`)
+
+})
+
+io.on('connection', (socket) => {
+    console.log("Un utilisateur s'est connecté");
+
+    socket.on('disconnect', () => {
+        console.log("Un utilisateur s'est déconnecté");
+    })
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+});
+
+
+server.listen('3000', () => {
+     console.log('Server Start port 3000')
+})
