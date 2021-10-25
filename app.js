3b3f2ef4b4c32ac043bae33698fd74b4fafa6faf
KevMaillard <86302957+KevMaillard@users.noreply.github.com>
2021-10-25 14:03:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const PORT = process.env.PORT || 8080;
-const host = '0.0.0.0';
+// const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`)
@@ -23,7 +23,7 @@ io.on('connection', (socket) => {
 });
 
 
-server.listen(PORT, host, () => {
+server.listen(PORT, () => {
      console.log('Server Start port 8080')
 })
 
