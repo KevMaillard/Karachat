256282a42e3bbd783e3e428ac11d76f54454bf1c
KevMaillard <86302957+KevMaillard@users.noreply.github.com>
2021-10-25 13:37:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const app = require('express');
+const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const PORT = process.env.PORT || 8080;
